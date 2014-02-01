da5f83a54cb7872c923f41700643e47e97ea484d
Selman Eser <basmimarsinan@hotmail.com>
2014-02-01 23:03:14 +0100

    Fixing some template classes.

--- library/modules/book/book.template.php
+++ library/modules/book/book.template.php
@@ -18,13 +18,13 @@ function template_book_list()
 	global $template;
 
 	echo '
-		<div book="page-header">
-			<div book="pull-right">
-				<a book="btn btn-warning" href="', build_url(array('book', 'edit')), '">Add Book</a>
+		<div class="page-header">
+			<div class="pull-right">
+				<a class="btn btn-warning" href="', build_url(array('book', 'edit')), '">Add Book</a>
 			</div>
 			<h2>Book List</h2>
 		</div>
-		<table book="table table-striped table-bordered">
+		<table class="table table-striped table-bordered">
 			<thead>
 				<tr>
 					<th>Name</th>
@@ -37,7 +37,7 @@ function template_book_list()
 	{
 		echo '
 				<tr>
-					<td book="align_center" colspan="2">There are not any books added yet!</td>
+					<td class="align_center" colspan="2">There are not any books added yet!</td>
 				</tr>';
 	}
 
@@ -46,9 +46,9 @@ function template_book_list()
 		echo '
 				<tr>
 					<td>', $book['name'], '</td>
-					<td book="span3 align_center">
-						<a book="btn btn-primary" href="', build_url(array('book', 'edit', $book['id'])), '">Edit</a>
-						<a book="btn btn-danger" href="', build_url(array('book', 'delete', $book['id'])), '">Delete</a>
+					<td class="span3 align_center">
+						<a class="btn btn-primary" href="', build_url(array('book', 'edit', $book['id'])), '">Edit</a>
+						<a class="btn btn-danger" href="', build_url(array('book', 'delete', $book['id'])), '">Delete</a>
 					</td>
 				</tr>';
 	}
@@ -63,18 +63,18 @@ function template_book_edit()
 	global $user, $template;
 
 	echo '
-		<form book="form-horizontal" action="', build_url(array('book', 'edit')), '" method="post">
+		<form class="form-horizontal" action="', build_url(array('book', 'edit')), '" method="post">
 			<fieldset>
 				<legend>', (!$template['book']['is_new'] ? 'Edit' : 'Add'), ' Book</legend>
-				<div book="control-group">
-					<label book="control-label" for="name">Name:</label>
-					<div book="controls">
-						<input type="text" book="input-xlarge" id="name" name="name" value="', $template['book']['name'], '" />
+				<div class="control-group">
+					<label class="control-label" for="name">Name:</label>
+					<div class="controls">
+						<input type="text" class="input-xlarge" id="name" name="name" value="', $template['book']['name'], '" />
 					</div>
 				</div>
-				<div book="form-actions">
-					<input type="submit" book="btn btn-primary" name="save" value="Save changes" />
-					<input type="submit" book="btn" name="cancel" value="Cancel" />
+				<div class="form-actions">
+					<input type="submit" class="btn btn-primary" name="save" value="Save changes" />
+					<input type="submit" class="btn" name="cancel" value="Cancel" />
 				</div>
 			</fieldset>
 			<input type="hidden" name="book" value="', $template['book']['id'], '" />
@@ -87,13 +87,13 @@ function template_book_delete()
 	global $user, $template;
 
 	echo '
-		<form book="form-horizontal" action="', build_url(array('book', 'delete')), '" method="post">
+		<form class="form-horizontal" action="', build_url(array('book', 'delete')), '" method="post">
 			<fieldset>
 				<legend>Delete Book</legend>
 				Are you sure you want to delete the book &quot;', $template['book']['name'], '&quot;?
-				<div book="form-actions">
-					<input type="submit" book="btn btn-danger" name="delete" value="Delete" />
-					<input type="submit" book="btn" name="cancel" value="Cancel" />
+				<div class="form-actions">
+					<input type="submit" class="btn btn-danger" name="delete" value="Delete" />
+					<input type="submit" class="btn" name="cancel" value="Cancel" />
 				</div>
 			</fieldset>
 			<input type="hidden" name="book" value="', $template['book']['id'], '" />
--- library/modules/subject/subject.template.php
+++ library/modules/subject/subject.template.php
@@ -18,13 +18,13 @@ function template_subject_list()
 	global $template;
 
 	echo '
-		<div subject="page-header">
-			<div subject="pull-right">
-				<a subject="btn btn-warning" href="', build_url(array('subject', 'edit')), '">Add Subject</a>
+		<div class="page-header">
+			<div class="pull-right">
+				<a class="btn btn-warning" href="', build_url(array('subject', 'edit')), '">Add Subject</a>
 			</div>
 			<h2>Subject List</h2>
 		</div>
-		<table subject="table table-striped table-bordered">
+		<table class="table table-striped table-bordered">
 			<thead>
 				<tr>
 					<th>Name</th>
@@ -37,7 +37,7 @@ function template_subject_list()
 	{
 		echo '
 				<tr>
-					<td subject="align_center" colspan="2">There are not any subjects added yet!</td>
+					<td class="align_center" colspan="2">There are not any subjects added yet!</td>
 				</tr>';
 	}
 
@@ -46,9 +46,9 @@ function template_subject_list()
 		echo '
 				<tr>
 					<td>', $subject['name'], '</td>
-					<td subject="span3 align_center">
-						<a subject="btn btn-primary" href="', build_url(array('subject', 'edit', $subject['id'])), '">Edit</a>
-						<a subject="btn btn-danger" href="', build_url(array('subject', 'delete', $subject['id'])), '">Delete</a>
+					<td class="span3 align_center">
+						<a class="btn btn-primary" href="', build_url(array('subject', 'edit', $subject['id'])), '">Edit</a>
+						<a class="btn btn-danger" href="', build_url(array('subject', 'delete', $subject['id'])), '">Delete</a>
 					</td>
 				</tr>';
 	}
@@ -63,18 +63,18 @@ function template_subject_edit()
 	global $user, $template;
 
 	echo '
-		<form subject="form-horizontal" action="', build_url(array('subject', 'edit')), '" method="post">
+		<form class="form-horizontal" action="', build_url(array('subject', 'edit')), '" method="post">
 			<fieldset>
 				<legend>', (!$template['subject']['is_new'] ? 'Edit' : 'Add'), ' Subject</legend>
-				<div subject="control-group">
-					<label subject="control-label" for="name">Name:</label>
-					<div subject="controls">
-						<input type="text" subject="input-xlarge" id="name" name="name" value="', $template['subject']['name'], '" />
+				<div class="control-group">
+					<label class="control-label" for="name">Name:</label>
+					<div class="controls">
+						<input type="text" class="input-xlarge" id="name" name="name" value="', $template['subject']['name'], '" />
 					</div>
 				</div>
-				<div subject="form-actions">
-					<input type="submit" subject="btn btn-primary" name="save" value="Save changes" />
-					<input type="submit" subject="btn" name="cancel" value="Cancel" />
+				<div class="form-actions">
+					<input type="submit" class="btn btn-primary" name="save" value="Save changes" />
+					<input type="submit" class="btn" name="cancel" value="Cancel" />
 				</div>
 			</fieldset>
 			<input type="hidden" name="subject" value="', $template['subject']['id'], '" />
@@ -87,13 +87,13 @@ function template_subject_delete()
 	global $user, $template;
 
 	echo '
-		<form subject="form-horizontal" action="', build_url(array('subject', 'delete')), '" method="post">
+		<form class="form-horizontal" action="', build_url(array('subject', 'delete')), '" method="post">
 			<fieldset>
 				<legend>Delete Subject</legend>
 				Are you sure you want to delete the subject &quot;', $template['subject']['name'], '&quot;?
-				<div subject="form-actions">
-					<input type="submit" subject="btn btn-danger" name="delete" value="Delete" />
-					<input type="submit" subject="btn" name="cancel" value="Cancel" />
+				<div class="form-actions">
+					<input type="submit" class="btn btn-danger" name="delete" value="Delete" />
+					<input type="submit" class="btn" name="cancel" value="Cancel" />
 				</div>
 			</fieldset>
 			<input type="hidden" name="subject" value="', $template['subject']['id'], '" />
